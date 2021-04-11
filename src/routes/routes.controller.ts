42c693f4a8db55ad80a7e4abec0d4e92b87abbfa
unknown <johnatasr@hotmail.com>
2021-04-11 16:08:40 -0300

    novas changes

--- src/routes/routes.controller.ts
+++ src/routes/routes.controller.ts
@@ -1,11 +1,32 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { 
+  Controller, 
+  Get, 
+  Post, 
+  Body, 
+  Patch, 
+  Param, 
+  Delete, 
+  Inject, 
+  OnModuleInit } from '@nestjs/common';
 import { RoutesService } from './routes.service';
 import { CreateRouteDto } from './dto/create-route.dto';
 import { UpdateRouteDto } from './dto/update-route.dto';
+import { ClientKafka, MessagePattern, Payload } from '@nestjs/microservices';
+import { Producer } from '@nestjs/microservices/external/kafka.interface';
+// import { RoutesGateway } from './routes.gateway';
 
 @Controller('routes')
-export class RoutesController {
-  constructor(private readonly routesService: RoutesService) {}
+export class RoutesController implements OnModuleInit {
+
+  private kafkaProducer: Producer
+
+  constructor(
+    private readonly routesService: RoutesService,
+    @Inject('KAFKA_SERVICE')
+    private kafkaClient: ClientKafka,
+    // private routeGateway: RouteGateway
+
+    ) {}
 
   @Post()
   create(@Body() createRouteDto: CreateRouteDto) {
@@ -31,4 +52,40 @@ export class RoutesController {
   remove(@Param('id') id: string) {
     return this.routesService.remove(+id);
   }
+
+  async onModuleInit() {
+    this.kafkaProducer = await this.kafkaClient.connect()
+  }
+
+  @Get(':id/start')
+  startRoute(@Param('id') id: string) {
+    console.log("estamos aqui haha")
+    this.kafkaProducer.send({
+      topic: 'new-direction',
+      messages: [
+        {
+          key: 'new-direction',
+          value: JSON.stringify({ routeId: id, clientId: '' })
+        },
+      ],
+    });
+
+    console.log("mesg enviada")
+  }
+
+  @MessagePattern('new-position')
+  consumePosition(
+    @Payload()
+    message: {
+      value: {
+        routeId: string;
+        clientId: string;
+        position: [number, number];
+        finished: boolean;
+      }
+    }
+  ){
+    console.log(message)
+  }
+
 }
